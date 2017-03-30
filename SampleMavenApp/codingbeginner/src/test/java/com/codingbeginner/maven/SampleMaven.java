539fb912b4590b6653cee674214a8c300b311493
CodingBeginners <codingbeginnerpoints@gmail.com>
2017-03-30 21:31:17 +0530

    Add files via upload

---
+++ SampleMavenApp/codingbeginner/src/test/java/com/codingbeginner/maven/SampleMaven.java
@@ -0,0 +1,14 @@
+package com.codingbeginner.maven;
+
+public class SampleMaven {
+
+
+
+	public static void main(String[] args) {
+	
+	
+		System.out.println("CodingBeginner Sample Project Run Successfully....");
+
+	}
+	
+}
